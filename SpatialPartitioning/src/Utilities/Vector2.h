7a33084d0c58c7a04ce1a7126a80be5c0e03df33
Jamie Buttenshaw <73542559+jambuttenshaw@users.noreply.github.com>
2021-11-10 14:58:37 +0000

    Implement Vector2 class

--- SpatialPartitioning/src/Utilities/Vector2.h
+++ SpatialPartitioning/src/Utilities/Vector2.h
@@ -7,7 +7,29 @@ public:
 	Vector2() = default;
 	~Vector2() = default;
 
+	float sqr_magnitude();
+	float magnitude();
+
+	void normalise();
+	Vector2 normalised();
+
+	float dot(const Vector2& rhs);
+
 	float x;
 	float y;
 };
 
+// Vector2 operators
+
+Vector2 operator+(Vector2 lhs, const Vector2& rhs);
+Vector2 operator-(Vector2 lhs, const Vector2& rhs);
+Vector2 operator*(Vector2 lhs, float rhs);
+Vector2 operator*(float lhs, const Vector2& rhs);
+Vector2 operator/(Vector2 lhs, float rhs);
+
+void operator+=(Vector2& lhs, const Vector2& rhs);
+void operator-=(Vector2& lhs, const Vector2& rhs);
+void operator*=(Vector2& lhs, float rhs);
+void operator/=(Vector2& lhs, float rhs);
+
+bool operator==(Vector2 lhs, const Vector2& rhs);
